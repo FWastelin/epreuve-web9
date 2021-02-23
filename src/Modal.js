783edf3b2a3ca1016ad79c5e1f6a8583da3016b4
fanny <fanny.wastelin@hotmail.com>
2021-02-23 16:24:27 +0100

    essai modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,8 +1,12 @@
 // modal ne fonctionne pas
-/*function Modal({props}){
+/*function Modal({date, entree, plat, dessert}){
     return(
         <div className='modal'>
             <h1>{props.title}</h1>
+            <p>{date}</p>
+            <p>Entrée:{entree}</p>
+            <p>Plat:{plat}</p>
+            <p>dessert:{dessert}</p>
             <p>Pour passer commande via What's app, composer le numéro: 06 66 666 666</p>
         </div>
         <button className='btn-fermer' onClick={ () => setModalVisible(false)}>Fermer</button>
